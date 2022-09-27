80adcd1011548a3b2eccfbfe39959e7b53c5f03a
Praise Nnamonu <prenonnamonu@gmail.com>
2022-09-27 07:28:27 +0100

    wrote function to get random computer choice, wrote function to get player choice, wrote function to produce a winner from the two choices

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-console.log("Hello World!");
+function getComputerChoice() {
+    let getComputerChoice = Math.random();
+    switch (true) {
+        case getComputerChoice < 0.34:
+            return "Rock";
+        case getComputerChoice <= 0.67:
+            return "Paper";
+        default:
+            return "Scissors";
+    }
+}
+
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+const playerSelection = prompt("Rock, Paper, or Scissors?", " ").toLowerCase();
+console.log(playerSelection);
+
+function firstRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "Paper") {
+        return "You lose! Paper beats Rock";
+    } else if (playerSelection === "paper" && computerSelection === "Scissors") {
+        return "You lose! Scissors beats Paper";
+    } else if (playerSelection === "scissors" && computerSelection === "Rock") {
+        return "You lose! Rock beats Scissors";
+    } else if (playerSelection === computerSelection.toLowerCase()) {
+        return "It's a fre*king tie!";
+    } else return "You win!"
+}
+console.log(firstRound(playerSelection, computerSelection));
